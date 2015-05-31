14df5358eaea5cab6b60b1397b23152896486b5c
Chaitanya Bhasme <chaitanya6790@gmail.com>
2015-05-31 17:48:51 -0400

    Added Solution to JAva Stack

---
+++ java/data/Paranthesis.java
@@ -0,0 +1,49 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Paranthesis {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext()) {
+			list.add(input.next());
+		}
+		for (String s : list) {
+			System.out.println(checkIfBalanced(s));
+		}
+	}
+
+	public static boolean checkIfBalanced(String s) {
+		char[] arr = s.toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		for (char c : arr) {
+			if (c == '{' || c == '[' || c == '(') {
+				stack.add(c);
+			} else {
+				if (!stack.isEmpty()) {
+					char tmp = stack.pop();
+					if (tmp == '{' && c == '}') {
+						continue;
+					} else if (tmp == '[' && c == ']') {
+						continue;
+					} else if (tmp == '(' && c == ')') {
+						continue;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			}
+		}
+		if (stack.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
